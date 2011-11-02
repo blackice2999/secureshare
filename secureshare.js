1768128bc23a1a152eb63cd8c20943526fb9a5bc
Helrunar <info@projektwaterkant.de>
2011-11-02 23:06:42 +0100

    set first variables

--- secureshare.js
+++ secureshare.js
@@ -1,7 +1,6 @@
 Drupal.behaviors.secureshare = function(context) {
   $('.socialshareprivacy:not(.processed)').addClass('processed').each(function() {
     var element = this;
-
     $(element).socialSharePrivacy(Drupal.settings.secureshare);
   });
 };
