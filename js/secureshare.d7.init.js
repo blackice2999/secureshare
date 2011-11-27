9400c344056d0d73cd20d9da88768efe76012791
blackice2999 <blackice2999@googlemail.com>
2011-11-27 09:38:23 +0100

    removed jquery default plugin encapsulate because we don't need them in drupal 7 (drupal.attachBehaviors already capsuled)
    Signed-off-by: blackice2999 <blackice2999@googlemail.com>

--- js/secureshare.d7.init.js
+++ js/secureshare.d7.init.js
@@ -1,17 +1,13 @@
-(function ($) {
+Drupal.behaviors.secureshare = {
+    attach:function (context, settings) {
+        "use strict";
 
-    Drupal.behaviors.secureshare = {
-        attach:function (context, settings) {
-            "use strict";
-
-            if ('secureshare' in settings) {
-                $.each(settings.secureshare, function(id, configuration){
-                    $('#' + id).once('secureshare', function () {
-                        $(this).socialSharePrivacy(configuration);
-                    });
+        if ('secureshare' in settings) {
+            $.each(settings.secureshare, function (id, configuration) {
+                $('#' + id).once('secureshare', function () {
+                    $(this).socialSharePrivacy(configuration);
                 });
-            }
+            });
         }
-    };
-
-})(jQuery);
+    }
+};
